9e6f57e518f80ecee1f17aebef2b56a7842c7ee2
Sharon Miller <sharon@sharonmillercreative.com>
2018-09-04 16:42:07 -0700

    greet test checking for no string is  null

--- test/greet.test.js
+++ test/greet.test.js
@@ -8,6 +8,10 @@ let expectedValue = 'hello world';
 expect(actual).toBe(expectedValue);
 });
 
-test('this test should return null if no args are passed', () => {
-  expect(greet.hello()).toBeNull();
+test('this test should return null if arg is not a string', () => {
+expect(greet.hello(1)).toBeNull();
+expect(greet.hello(true)).toBeNull();
+expect(greet.hello([])).toBeNull();
+expect(greet.hello({})).toBeNull();
+
 });
